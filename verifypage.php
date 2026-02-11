bd833257910f68dfaff6ef43f8c672414b4b82a6
Giancarlo Lawan <lawangiancarlo10@gmail.com>
2026-02-11 21:05:56 +0800

    Removed APIs

--- verifypage.php
+++ verifypage.php
@@ -77,7 +77,6 @@ if (isset($_POST["semail"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="design/verifypage-style.css">
-    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap" rel="stylesheet">
     <title>Login</title>
 </head>
 
