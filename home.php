20cac2ba6a171490f26de44bd53a23356f5eefaf
Giancarlo Lawan <lawangiancarlo10@gmail.com>
2026-02-12 13:12:27 +0800

    Added initial components for home.php

--- home.php
+++ home.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="design/home-style.css" />
     <title>Home</title>
 </head>
 <body>
-    
+    <header>hoidas</header>
+    <nav>fsadas</nav>
+    <section>asdsa</section>
+    <aside>cashudaso</aside>
+    <footer>adsaasd</footer>
 </body>
 </html>
