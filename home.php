596c3e80103a2d090488a7a919b0de7cce597d48
Giancarlo Lawan <lawangiancarlo10@gmail.com>
2026-02-12 17:37:36 +0800

    Refactor stylesheets: move CSS files to 'stylesheets' directory and update links in HTML files

--- home.php
+++ home.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="design/home-style.css" />
+    <link rel="stylesheet" href="stylesheets/home.css" />
     <title>Home</title>
 </head>
 <body>
--- verifypage.php
+++ verifypage.php
@@ -76,7 +76,7 @@ if (isset($_POST["semail"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="design/verifypage-style.css">
+    <link rel="stylesheet" href="stylesheets/verifypage.css">
     <title>Login</title>
 </head>
 
