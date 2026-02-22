ac102cad3217938468bc8c6735a4923acb229729
Giancarlo Lawan <lawangiancarlo10@gmail.com>
2026-02-23 01:39:18 +0800

    Implement login page: create loginpage.php and update styles in loginpage.css

--- loginpage.php
+++ loginpage.php
@@ -1,4 +1,4 @@
-<!--loginpage.html-->
+<!--loginpage.php-->
 
 <!doctype html>
 <html lang="en">
@@ -10,21 +10,30 @@
   </head>
 
   <body>
-    <form action="verifypage.php" method="post" onsubmit="disableButton()">
+    <form action="loginpage.php" method="post" onsubmit="disableButton()">
       <div id="container">
         <div id="login">Login</div>
         <div id="input">
           <label for="email">Email:</label>
           <input
             type="email"
-            name="semail"
-            id="email"
+            name="email"
+            class="input-field"
             pattern="^[a-z._]+@dlsu\.edu\.ph$"
             placeholder="email@dlsu.edu.ph"
             title="Use lowercase letters and underscores only (name_name@dlsu.edu.ph)"
             required
           />
-          <input type="submit" value="Verify" id="submitbtn" />
+          <label for="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            class="input-field"
+            placeholder="Enter your password"
+            title="Enter your password"
+            required
+          />
+          <input type="submit" value="Login" id="submitbtn" />
         </div>
       </div>
     </form>
