1778b1ca1078572499da3ee25cb9d7aa95d8d03f
Giancarlo Lawan <lawangiancarlo10@gmail.com>
2026-02-24 00:50:55 +0800

    Refactor database connection to use mysqli and improve error handling

--- database.php
+++ database.php
@@ -1,14 +1,14 @@
 <?php
 $db_server = "localhost";
-$db_user = "root"; // change details based on your credentials
-$db_password = "1234"; // change details based on your credentials
+$db_user = "root";
+$db_password = "1234";
 $db_name = "itprogmp_db";
-$conn = new mysqli($db_server, $db_user, $db_password, $db_name);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$conn = mysqli_connect($db_server, $db_user, $db_password, $db_name);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 
-// set charset
-$conn->set_charset("utf8mb4");
+mysqli_set_charset($conn, "utf8mb4");
 ?>
