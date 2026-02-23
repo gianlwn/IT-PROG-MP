c5de912acc444f5947a212083425fe0ab1df7f67
Giancarlo Lawan <lawangiancarlo10@gmail.com>
2026-02-24 02:21:34 +0800

    Add verification page and create user profile files with basic structure

--- verifypage.php
+++ verifypage.php
@@ -75,7 +75,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="stylesheets/verifypage.css">
-    <title>Login</title>
+    <title>Verify</title>
 </head>
 
 <body>
