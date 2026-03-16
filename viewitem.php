11c35f8310e9e59e08c24605312317939771ef1b
Giancarlo Lawan <lawangiancarlo10@gmail.com>
2026-03-17 02:34:57 +0800

    Added skeleton code

---
+++ viewitem.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+include 'db.php';
+
+// check if user is logged in
+if (!isset($_SESSION["user_id"])) {
+    header("Location: loginpage.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DLSU Marketplace | <?php echo ""; ?></title>
+</head>
+<body>
+    
+</body>
+</html>
