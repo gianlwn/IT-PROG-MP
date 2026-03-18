50c0fa02b5c4ab1c3936e4c8d40f71bc7ab5d3d7
Giancarlo Lawan <lawangiancarlo10@gmail.com>
2026-03-18 15:12:30 +0800

    Finished backend

--- viewitem.php
+++ viewitem.php
@@ -23,17 +23,17 @@ $profile_pic = $_SESSION["profile_picture"] ?? "login-icon.png";
 $pic_path = "images/" . $profile_pic;
 
 $listing_id = intval($_GET["listing_id"]);
-$item_query = "SELECT l.listing_id, c1.category_name AS cat1, c2.category_name AS cat2, c3.category_name AS cat3,
-                         l.product_name, CONCAT(u.first_name, ' ', u.last_name) AS full_name, IFNULL(ROUND(AVG(r.rating_value), 1), 0) AS avg_rating,
-                         l.price, l.quantity, (SELECT image_path FROM listing_images WHERE listing_id = l.listing_id LIMIT 1) AS image_path
-                  FROM listings l
-                  LEFT JOIN users u ON u.user_id = l.seller_id
-                  LEFT JOIN ratings r ON r.rated_user_id = u.user_id
-                  LEFT JOIN categories c1 ON c1.category_id = l.category1_id
-                  LEFT JOIN categories c2 ON c2.category_id = l.category2_id
-                  LEFT JOIN categories c3 ON c3.category_id = l.category3_id
-                  WHERE l.listing_id = $listing_id
-                  GROUP BY l.listing_id";
+$item_query = "SELECT c1.category_name AS cat1, c2.category_name AS cat2, c3.category_name AS cat3,
+                      CONCAT(u.first_name, ' ', u.last_name) AS seller_name, IFNULL(ROUND(AVG(r.rating_value), 1), 0) AS avg_rating,
+                      l.listing_id, l.product_name, l.price, l.quantity, l.description, l.status
+                FROM listings l
+                LEFT JOIN users u ON u.user_id = l.seller_id
+                LEFT JOIN ratings r ON r.rated_user_id = u.user_id
+                LEFT JOIN categories c1 ON c1.category_id = l.category1_id
+                LEFT JOIN categories c2 ON c2.category_id = l.category2_id
+                LEFT JOIN categories c3 ON c3.category_id = l.category3_id
+                WHERE l.listing_id = '$listing_id'
+                GROUP BY l.listing_id";
 
 $item_result = $conn->query($item_query);
 
@@ -46,10 +46,24 @@ if ($item_result->num_rows == 0) {
 $item = $item_result->fetch_assoc();
 
 // get all images for this listing
+$images = [];
+$image_query = "SELECT image_path
+                FROM listing_images
+                WHERE listing_id = '$listing_id'";
+$image_result = $conn->query($item_query);
+
+if ($image_result == TRUE && $image_result->num_rows > 0) {
+    while ($img_row = $image_result->fetch_assoc()) {
+        $images[] = $img_row['image_path'];
+    }
+}
 
 // get all the categories for this listng
-
-// get the main image
+$categories = [];
+if (!empty($item["cat1"])) $categories[] = $item["cat1"];
+if (!empty($item["cat2"])) $categories[] = $item["cat2"];
+if (!empty($item["cat3"])) $categories[] = $item["cat3"];
+$category_display = implode(', ', $categories);
 ?>
 
 <!DOCTYPE html>
